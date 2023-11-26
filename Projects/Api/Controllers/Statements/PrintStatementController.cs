e799815a9a1ac89df65b5279599cef308efec56b
MustafaGaber <mus.gaber@gmail.com>
2023-11-26 11:01:18 +0200

    PrintStatement controller

---
+++ Projects/Api/Controllers/Statements/PrintStatementController.cs
@@ -0,0 +1,85 @@
+﻿using jsreport.AspNetCore;
+using jsreport.Binary;
+using jsreport.Local;
+using jsreport.Types;
+using Microsoft.AspNetCore.Mvc;
+using NUCA.Projects.Application.Statements.Commands.CreateStatement;
+using NUCA.Projects.Application.Statements.Commands.UpdateStatement;
+using NUCA.Projects.Application.Statements.Queries.GetProjectStatements;
+using NUCA.Projects.Application.Statements.Queries.GetStatement;
+using NUCA.Projects.Application.Statements.Queries.GetUserStatements;
+using NUCA.Projects.Application.Statements.Queries.PrintStatement;
+
+namespace NUCA.Projects.Api.Controllers.Statements
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrintStatementController : Controller
+    {
+        private readonly IPrintStatementQuery _printStatementQuery;
+        private readonly IJsReportMVCService _jsReportMVCService;
+        public PrintStatementController( IJsReportMVCService jsReportMVCService, IPrintStatementQuery printStatementQuery)
+        {
+            _jsReportMVCService = jsReportMVCService;
+            _printStatementQuery = printStatementQuery;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Print(long id)
+        {
+            var model = await _printStatementQuery.Execute(id);
+            var header = await _jsReportMVCService.RenderViewToStringAsync(HttpContext, RouteData, "Header", model);
+            var footer = await _jsReportMVCService.RenderViewToStringAsync(HttpContext, RouteData, "Footer", new { });
+            var content = await _jsReportMVCService.RenderViewToStringAsync(HttpContext, RouteData, "Statement", model.Statement);
+            var pdfOperations = await _jsReportMVCService.RenderViewToStringAsync(HttpContext, RouteData, "PdfOperations", model.Statement.Withholdings);
+            var rs = new LocalReporting().UseBinary(JsReportBinary.GetBinary()).AsUtility().Create();
+            var report = await rs.RenderAsync(new RenderRequest()
+            {
+                Data = model,
+                Template = new Template()
+                {
+                    Recipe = Recipe.ChromePdf,
+                    Engine = Engine.Handlebars,
+                    Content = content,
+                    Chrome = new Chrome()
+                    {
+                        DisplayHeaderFooter = true,
+                        HeaderTemplate = header,
+                        FooterTemplate = footer,
+                        Landscape = true,
+                        MarginTop = "3cm",
+                        MarginLeft = "1cm",
+                        MarginBottom = "2.5cm",
+                        MarginRight = "1cm",
+                    },
+                    PdfOperations =
+                    new List<PdfOperation> {
+                        new PdfOperation()
+                        {
+                            Type = PdfOperationType.Merge,
+                            MergeWholeDocument = true,
+                            Template = new Template()
+                              {
+                                  Recipe = Recipe.ChromePdf,
+                                  Engine = Engine.Handlebars,
+                                  Chrome = new Chrome()
+                                  {
+                                        Landscape = true,
+                                        MarginTop = "2.5cm",
+                                        MarginLeft = "1cm",
+                                        MarginBottom = "2.5cm",
+                                        MarginRight = "1cm",
+                                  },
+                                  Content = pdfOperations,
+                                },
+                            },
+                    },
+                },
+            });
+            var memoryStream = new MemoryStream();
+            await report.Content.CopyToAsync(memoryStream);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            return new FileStreamResult(memoryStream, "application/pdf");
+        }
+    }
+}
--- Projects/Api/Controllers/Statements/StatementsController.cs
+++ Projects/Api/Controllers/Statements/StatementsController.cs
@@ -23,85 +23,15 @@ namespace NUCA.Projects.Api.Controllers.Statements
         private readonly IGetStatementQuery _getStatementQuery;
         private readonly IGetCurrentStatementsQuery _getCurrentStatementsQuery;
         private readonly IGetProjectStatementsQuery _getProjectStatementsQuery;
-        private readonly IPrintStatementQuery _printStatementQuery;
         private readonly ICreateStatementCommand _createCommand;
         private readonly IUpdateStatementCommand _updateStatementCommand;
-        private readonly IJsReportMVCService _jsReportMVCService;
-        public StatementsController(IGetStatementQuery getStatementQuery, IGetCurrentStatementsQuery getCurrentStatementsQuery, IGetProjectStatementsQuery getProjectStatementsQuery, ICreateStatementCommand createCommand, IUpdateStatementCommand updateStatementCommand, IJsReportMVCService jsReportMVCService, IPrintStatementQuery printStatementQuery)
+        public StatementsController(IGetStatementQuery getStatementQuery, IGetCurrentStatementsQuery getCurrentStatementsQuery, IGetProjectStatementsQuery getProjectStatementsQuery, ICreateStatementCommand createCommand, IUpdateStatementCommand updateStatementCommand)
         {
             _getStatementQuery = getStatementQuery;
             _getCurrentStatementsQuery = getCurrentStatementsQuery;
             _getProjectStatementsQuery = getProjectStatementsQuery;
             _createCommand = createCommand;
             _updateStatementCommand = updateStatementCommand;
-            _jsReportMVCService = jsReportMVCService;
-            _printStatementQuery = printStatementQuery;
-        }
-
-        [HttpGet("Print/{id}")]
-        //[MiddlewareFilter(typeof(JsReportPipeline))]
-        public async Task<IActionResult> Print(long id)
-        {
-            var model = await _printStatementQuery.Execute(id);
-            var header = await _jsReportMVCService.RenderViewToStringAsync(HttpContext, RouteData, "Header", model);
-            var footer = await _jsReportMVCService.RenderViewToStringAsync(HttpContext, RouteData, "Footer", new { });
-            var content = await _jsReportMVCService.RenderViewToStringAsync(HttpContext, RouteData, "Statement", model.Statement);
-            var rs = new LocalReporting().UseBinary(JsReportBinary.GetBinary()).AsUtility().Create();
-            var report = await rs.RenderAsync(new RenderRequest()
-            {
-                Data = model,
-                Template = new Template()
-                {
-                    Recipe = Recipe.ChromePdf,
-                    Engine = Engine.Handlebars,
-                    Content = content,
-                    Chrome = new Chrome()
-                    {
-                        DisplayHeaderFooter = true,
-                        HeaderTemplate = header,
-                        FooterTemplate = footer,
-                        Landscape = true,
-                        MarginTop = "3cm",
-                        MarginLeft = "1cm",
-                        MarginBottom = "2cm",
-                        MarginRight = "1cm",
-                    },
-                    PdfOperations =
-                    new List<PdfOperation> {
-                        new PdfOperation()
-                        {
-                            Type = PdfOperationType.Merge,
-                            MergeWholeDocument = true,
-                            Template = new Template()
-                              {
-                                  Recipe = Recipe.ChromePdf,
-                                  Engine = Engine.Handlebars,
-                                  Chrome = new Chrome()
-                                  {
-                                        Landscape = true,
-                                        MarginTop = "2.5cm",
-                                        MarginLeft = "1cm",
-                                        MarginBottom = "2cm",
-                                        MarginRight = "1cm",
-                                  },
-                                  Content =
-                                  @"{{#each $pdf.pages}}
-                                        <div dir=""rtl"">
-                                            {{#if 1}}
-                                              <div style=""page-break-before: always;""></div>
-                                            {{/if}}     
-                                            {{group}}
-                                        </div>
-                                    {{/each}}",
-                                },
-                            },
-                    },
-                },
-            });
-            var memoryStream = new MemoryStream();
-            await report.Content.CopyToAsync(memoryStream);
-            memoryStream.Seek(0, SeekOrigin.Begin);
-            return new FileStreamResult(memoryStream, "application/pdf");
         }
 
         [HttpGet("ProjectStatements/{projectId}")]
